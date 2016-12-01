42d926a8cd71a5ca356a3cb3c701947f49f2de74
AntonAlexandrov <antond_aleksandrov@abv.bg>
2016-12-01 19:31:08 +0200

    The first somewhat working version

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/4c/c04362beeab700161130cb1ba432aac8
@@ -0,0 +1,6 @@
+
+public class MultithreadingApplicationTester {
+	public static void main(String args[]){
+		System.out.println("Hello friend! :)");
+	}
+}
--- MultithreadingApplication/src/MultithreadingApplicationTester.java
+++ MultithreadingApplication/src/MultithreadingApplicationTester.java
@@ -1,6 +1,9 @@
+import algorithm.search.LinearSearch;
 
 public class MultithreadingApplicationTester {
 	public static void main(String args[]){
-		System.out.println("Hello friend! :)");
+		LinearSearch linearSearch = new LinearSearch("LinearSearch");
+		
+		linearSearch.start();
 	}
 }
